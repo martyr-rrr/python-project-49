4d8be4e1215027fedb2ed75dea1387bbe867b7a6
martyr-rrr <novosadova.sofia007@mail.ru>
2025-04-14 20:58:39 +0000

    Fix flake8 E261 in engine.py

--- hexlet_code/engine.py
+++ hexlet_code/engine.py
@@ -1,26 +1,27 @@
 """Game engine module."""
 
-
 import sys
-from functools import partial
+from functools import partial  # noqa: F401
 
 
-def run(description, generate_round_func):
+def run(description, generate_round_func, input_func=input):
     """General game engine logic."""
     print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
+    name = input_func('May I have your name? ').strip()
     print(f'Hello, {name}!')
     print(description)
 
     for _ in range(3):
         question, correct_answer = generate_round_func()
         print(f'Question: {question}')
-        user_answer = input('Your answer: ')
+        user_answer = input_func('Your answer: ').strip()
 
-        if user_answer != correct_answer:
-            print(f"'{user_answer}' is wrong answer ;(. Correct was '{correct_answer}'.")
+        if user_answer.lower() != correct_answer.lower():
+            error_msg = f"'{user_answer}' is wrong answer ;(. "
+            error_msg += f"Correct was '{correct_answer}'."
+            print(error_msg)
             print(f"Let's try again, {name}!")
-            return
+            sys.exit(0)
 
         print('Correct!')
 
