ff25485fdb87616b361253577122f26a0a972c5b
martyr-rrr <novosadova.sofia007@mail.ru>
2025-10-17 09:17:53 +0000

    Fix brain-games output to match test requirements

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,6 +3,9 @@
 def main():
     """Main function of the Brain Games."""
     print("Welcome to the Brain Games!")
+    print("May I have your name? ", end="")
+    name = input()
+    print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
