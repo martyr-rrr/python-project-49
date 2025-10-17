ceb66b9560178f283513c4b6ab9c921cd1e7066d
martyr-rrr <novosadova.sofia007@mail.ru>
2025-10-17 09:52:30 +0000

    Configure ruff linter and format code

--- Makefile
+++ Makefile
@@ -9,3 +9,9 @@ build:
 
 package-install:
 	uv tool install dist/*.whl
+
+lint:
+	uv run ruff check brain_games
+
+format:
+	uv run ruff format brain_games
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,5 +4,5 @@ import prompt
 
 def welcome_user():
     """Ask user name and greet."""
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
